7207c9ab90850e99caf0910fca48e61b920f4b13
russ_zhong <russ_zhong@kingdee.com>
2018-12-10 17:27:26 +0800

    add CI

---
+++ index.test.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: Russ Zhong 
+ * @Date: 2018-12-10 16:56:08 
+ * @Last Modified by:   Russ Zhong 
+ * @Last Modified time: 2018-12-10 16:56:08 
+ */
---
+++ src/packages/Util.js
@@ -0,0 +1,6 @@
+/*
+ * @Author: Russ Zhong 
+ * @Date: 2018-12-10 17:13:16 
+ * @Last Modified by:   Russ Zhong 
+ * @Last Modified time: 2018-12-10 17:13:16 
+ */
